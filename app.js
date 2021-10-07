cc65351156ddb1c74434365a45dd04568f25788e
leyosu23 <leyosu23@gmail.com>
2021-10-07 19:32:53 -0400

    Implement default route and partial & layout

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
+app.get("/", function (req, res) {
+  res.render('home', {
+    content: homeStartingContent
+  });
+})
 
 
 
@@ -27,7 +34,6 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
